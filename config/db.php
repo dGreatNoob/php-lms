0867347fc3c7a9d4939d812c8fd1bcd0a4c3079d
dGreatNoob <beltesheyzel@gmail.com>
2025-05-18 15:58:26 +0800

    uncommented echo message

--- config/db.php
+++ config/db.php
@@ -6,9 +6,9 @@ $dbname = "math_gineer_db";
 
 $conn = new mysqli($host, $user, $pass, $dbname);
 
-// if ($conn->connect_error) {
-//     die("<div class='message error'>Check Database Connection: " . $conn->connect_error . "</div>");
-// } else {
-//     echo "<div class='message success'>You are now connected to Math Gineer!</div>";
-// }
+ if ($conn->connect_error) {
+    die("<div class='message error'>Check Database Connection: " . $conn->connect_error . "</div>");
+ } else {
+     echo "<div class='message success'>You are now connected to Math Gineer!</div>";
+ }
 
